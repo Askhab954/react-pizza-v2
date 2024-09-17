8b9219d90e0613154b4435d7ca2d7eadb73e79ff
Askhab954 <kagermanov.861@mail.ru>
2024-09-17 16:52:23 +0300

    Cart item

--- src/components/Cart/СartItem.jsx
+++ src/components/Cart/СartItem.jsx
@@ -1,15 +1,14 @@
 import React from 'react'
 
-const CartItem = (id, title, type, size, price, count, imageUrl) => {
+const CartItem = (item, id, title, type, size, price, count, imageUrl) => {
   return (
-    <div>
-        <div className="cart__item">
+    <div className='cart__item-obj'>
   <div className="cart__item-img">
-    <img className="pizza-block__image" src={imageUrl} alt="Pizza"/>
+    <img className="pizza-block__image" src={item.imageUrl} alt="Pizza"/>
   </div>
   <div className="cart__item-info">
-    <h3>{title}</h3>
-    <p>{type}, 26 см.</p>
+    <h3>{item.title}</h3>
+    <p>{item.type}, 26 см.</p>
   </div>
   <div className="cart__item-count">
     <div className="button button--outline button--circle cart__item-count-minus">
@@ -29,7 +28,7 @@ const CartItem = (id, title, type, size, price, count, imageUrl) => {
     </div>
   </div>
   <div className="cart__item-price">
-    <b>{price} ₽</b>
+    <b>{item.price} ₽</b>
   </div>
   <div className="cart__item-remove">
     <div className="button button--outline button--circle">
@@ -41,7 +40,6 @@ const CartItem = (id, title, type, size, price, count, imageUrl) => {
     </div>
   </div>
             </div>
-    </div>
   )
 }
 
